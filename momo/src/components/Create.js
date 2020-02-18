d9cafa5dcfbd56fe5e6ba54479e936561a634fa2
Fayçal BORDJAH <fbordjah@linagora.com>
2020-02-18 17:09:16 +0100

    Improve Create

--- momo/src/components/Create.js
+++ momo/src/components/Create.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 const Create = () => {
 
     const [contact, setContact] = useState({
@@ -10,7 +11,6 @@ const Create = () => {
         email: '',
     })
 
-
     const onChange = (e) => {
         setContact({
             ...contact,
@@ -18,13 +18,9 @@ const Create = () => {
         })
     }
 
-
     const onSubmit = (event) => {
         event.preventDefault();
         console.log(contact);
-
-
-
         axios.post("http://localhost:8080/contacts", contact).then(
             (res) => {
                 console.log(res.data);
@@ -34,6 +30,11 @@ const Create = () => {
 
     return (
         <div className="container">
+            <div>
+                <Link to="/">
+                    <span className="glyphicon glyphicon-plus-sign" aria-hidden="true">Contacts</span>
+                </Link>
+            </div>
             <div className="panel panel-default">
                 <div className="panel-heading">
                     <h2 className="panel-title">
@@ -43,30 +44,29 @@ const Create = () => {
                 <div className="panel-body">
                     <form onSubmit={onSubmit}>
                         <div className="form-group">
-                            <label>Name: </label>
-                            <input type="text" name="name" placeholder="Your name" onChange={onChange} value={contact.name} />
+                            <label >Name: </label>
+                            <input type="text" className="form-control" name="name" placeholder="Your name" onChange={onChange} value={contact.name} />
                         </div>
                         <div className="form-group">
                             <label>Address: </label>
-                            <input type="text" name="address" placeholder="Your address" onChange={onChange} value={contact.address} />
+                            <input type="text" className="form-control" name="address" placeholder="Your address" onChange={onChange} value={contact.address} />
                         </div>
                         <div className="form-group">
                             <label>City: </label>
-                            <input type="text" name="city" placeholder="Your city" onChange={onChange} value={contact.city} />
+                            <input type="text" className="form-control" name="city" placeholder="Your city" onChange={onChange} value={contact.city} />
                         </div>
                         <div className="form-group">
                             <label>Phone: </label>
-                            <input type="text" name="phone" placeholder="Your phone" onChange={onChange} value={contact.phone} />
+                            <input type="text" className="form-control" name="phone" placeholder="Your phone" onChange={onChange} value={contact.phone} />
                         </div>
                         <div className="form-group">
                             <label>Email: </label>
-                            <input type="text" name="email" placeholder="Your email" onChange={onChange} value={contact.email} />
+                            <input className="form-control" type="text" name="email" placeholder="Your email" onChange={onChange} value={contact.email} />
                         </div>
-                        <button type="submit" className="btn btn-default"> Create</button>
+                        <button type="submit" className="btn btn-primary"> Create</button>
                     </form>
                 </div>
             </div>
-
         </div >);
 }
 export default Create;
